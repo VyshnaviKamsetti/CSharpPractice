e3393f2e9cd4fcb241ee7753613efbf092ac4b36
Vyshnavi Kamsetti <vyshnavi.kamsetti@meridianlink.com>
2022-08-26 02:28:04 -0700

    Struct and static classes Practice

---
+++ Struct/Staticclass/Practicingstatic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Staticclass
+{
+    public class Practicingstatic
+    {
+        
+        
+        public static decimal CelsiusToFahrenhet(string temperatureCelsius)
+        {
+            decimal celsius = decimal.Parse(temperatureCelsius);
+            decimal Fahrenheat = (celsius * 9 / 5) + 32;
+            return Fahrenheat;
+        }
+
+        public static decimal FahrenheatToCelsisu(string temperatureFahrenheit)
+        {
+            decimal Fahrenheat = decimal.Parse(temperatureFahrenheit);
+            decimal celsius = (Fahrenheat - 32) * 5 / 9;
+            return celsius;
+        }
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Staticclass;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("1. From Celsius to Fahrenheit.");
+            Console.WriteLine("2. From Fahrenheit to Celsius.");
+
+            string option = Console.ReadLine();
+            decimal F, C = 0;
+
+            switch (option)
+            {
+                case "1":
+                    Console.Write("Please enter the Celsius temperature: ");
+                    F = Practicingstatic.CelsiusToFahrenhet(Console.ReadLine());
+                    Console.WriteLine("Fahrenheit:{0:F2}", F);
+                    break;
+
+                case "2":
+                    Console.Write("Please enter the Fahrenheit temperature: ");
+                    F = Practicingstatic.FahrenheatToCelsisu(Console.ReadLine());
+                    Console.WriteLine("Celsius: {0:F2}", C);
+                    break;
+            }
+        }
+    }
+}
